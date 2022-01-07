1da85ece305601dc6c38d44b745cb0400e5052a3
nesty156 <nesty.wildstar@gmail.com>
2022-01-07 12:55:10 +0100

    Added effects end shooting

--- Assets/Laser.cs
+++ Assets/Laser.cs
@@ -7,6 +7,14 @@ public class Laser : MonoBehaviour
     public GameObject targetPrefab;
     private GameObject target;
     private LineRenderer lr;
+    
+    public float shootRate;
+    private float m_shootRateTimeStamp;
+
+    public GameObject m_shotPrefab;
+    
+    RaycastHit hit;
+    public float range = 30.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,20 +26,47 @@ public class Laser : MonoBehaviour
     void Update()
     {
         lr.SetPosition(0,transform.position);
-        RaycastHit hit;
         if (Physics.Raycast(transform.position, transform.forward, out hit))
         {
             if (hit.collider)
             {
                 lr.SetPosition(1, hit.point);
-                target.transform.position = new Vector3(hit.point.x,hit.point.y + 0.3f ,hit.point.z);
+                target.transform.position = new Vector3(hit.point.x,hit.point.y,hit.point.z);
             }
         }
         else
         {
-            lr.SetPosition(1,transform.position);
+            lr.SetPosition(1, transform.forward*100);
             target.transform.position = new Vector3(0,-10.0f,0);
         }
         
+        if (Input.GetMouseButton(0))
+        {
+            if (Time.time > m_shootRateTimeStamp)
+            {
+                shootRay();
+                m_shootRateTimeStamp = Time.time + shootRate;
+            }
+        }
+        
     }
+    
+    void shootRay()
+    {
+        //Ray ray = new Ray (transform.position, transform.forward);
+        if (Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+            GameObject laser = GameObject.Instantiate(m_shotPrefab, transform.position, transform.rotation) as GameObject;
+            laser.GetComponent<ShotBehavior>().setTarget(hit.point);
+            GameObject.Destroy(laser, 2f);
+            
+        }
+        else
+        {
+            GameObject laser = GameObject.Instantiate(m_shotPrefab, transform.position, transform.rotation) as GameObject;
+            laser.GetComponent<ShotBehavior>().setTarget(transform.position + transform.forward * range);
+            GameObject.Destroy(laser, 2f);
+        }
+    }
+    
 }
